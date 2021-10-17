d0fddb4eda8ea8c34f2a1ce76f7c4f7113adf1a2
Loris DUCAMPS <lducamps@dydu.ai>
2021-10-17 22:16:59 +0200

    test

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -9,12 +9,8 @@
             </div>
             <picture class="hero__image">
                 <!-- DESKTOP -->
-                <!-- <source media="(min-width: 992px)"
-                    :srcset="`${phones.phoneDesktop} 1x, ${phones.phoneDesktop2x} 2x`"
-                    type="image/png" alt="phone"> -->
-
-                    <img :srcset="`${phones.phoneDesktop} 1x, ${phones.phoneDesktop2x} 2x`"
-                    size="(max-width: 990px) 20vw, (max-width: 37.5em) 30vw, 300px"
+                <source media="(min-width: 992px)"
+                    :srcset="require('../assets/images/phone/phone-desktop')+ '1x,' + require('../assets/images/phone/phone-desktop-2x') + '2x'"
                     type="image/png" alt="phone">
 
                 <!-- TABLET -->
@@ -26,7 +22,7 @@
                     srcset="../assets/images/phone/phone-mobile.png 1x, ../assets/images/phone/phone-mobile-2x.png 2x"
                     type="image/png" alt="phone"> -->
 
-                <!-- <img :src="`${phoneDesktop}`" alt="Phone PayApi"> -->
+                <img :src="`${phoneDesktop}`" alt="Phone PayApi">
             </picture>
         </div>
     </section>
@@ -38,24 +34,6 @@
         components: {
             EmailInput
         },
-        data() {
-            return {
-                phones: {
-                    phoneDesktop: require('../assets/images/phone/phone-desktop.png'),
-                    phoneDesktop2x: require('../assets/images/phone/phone-desktop-2x.png'),
-                    phoneTablet: require('../assets/images/phone/phone-tablet.png'),
-                    phoneTablet2x: require('../assets/images/phone/phone-tablet-2x.png'),
-                    phoneMobile: require('../assets/images/phone/phone-mobile.png'),
-                    phoneMobile2x: require('../assets/images/phone/phone-mobile-2x.png'),
-                }
-            }
-        },
-        computed: {
-            images() {
-                return this.phones;
-            }
-        }
-
     }
 </script>
 
